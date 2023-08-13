9ff0ecd34451f99c5931d928ffb40f64960df5e8
root <nowayofway@gmail.com>
2023-08-13 14:38:27 +0000

    update

---
+++ .bash_history
@@ -0,0 +1,56 @@
+exit
+ls
+cd
+git clone https://github.com/EphemeralSapient/flask-hello-world
+apt update
+apt install git python3 python3-env gh
+apt install git python3 python3.10-env gh
+apt install git python3 python3-nenv gh
+apt install git python3 python3-venv gh
+ls
+git clone https://github.com/EphemeralSapient/flask-hello-world
+echo "# backend-prism" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/EphemeralSapient/backend-prism.git
+git push -u origin main
+gh
+gh login
+gh auth
+gh auth login
+ls
+rm RE
+rm README.md 
+ls -lah
+rm .git -rf
+echo "# backend-prism" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/EphemeralSapient/backend-prism.git
+git push -u origin main
+gh auth login
+git push -u origin main
+git remote add origin https://github.com/EphemeralSapient/backend-prism.git
+git commit -m "first commit"
+git push -u origin main
+nano
+apt install nano
+nano main.py
+nano requireme
+nts.txt
+nano requirements.txt
+pip freeze > requirements.txt
+apt install python3-pip
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+ls
+ls venv/
+ls venv/*
+pip freeze > requirements.txt
+pip install -r requirements.txt
+exit
---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import base64
+import json
+import os
+import face_recognition
+
+app = Flask(__name__)
+
+@app.route("/")
+def idk():
+    return "home page lol"
+
+@app.route('/upload', methods=['POST'])
+def upload_images():
+    try:
+        data = request.get_json()
+        if 'images' in data:
+            images_data = data['images']
+            uploaded_images = []
+
+            for idx, image_data in enumerate(images_data):
+                base64_image = image_data.get('image', '')
+                if base64_image:
+                    image_bytes = base64.b64decode(base64_image)
+                    
+                    # Save the image to a local directory
+                    image_filename = f'image_{idx}.png'
+                    image_path = os.path.join('uploads', image_filename)
+                    with open(image_path, 'wb') as f:
+                        f.write(image_bytes)
+
+                    uploaded_images.append({'status': 'uploaded', 'filename': image_filename})
+
+            return jsonify({'message': 'Images uploaded successfully', 'uploaded_images': uploaded_images}), 200
+        else:
+            print("wat, no images?")
+            return jsonify({'error': 'No images found in request'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == "__main__":
+    os.makedirs('uploads', exist_ok=True)  # Create the 'uploads' directory if it doesn't exist
+    app.run(host='0.0.0.0', port=8080)
